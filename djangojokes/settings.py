a62e7d00eef7a876793f4f4f7be93e2ab7f0104e
jramizares <jramizares@gmail.com>
2025-01-15 08:53:54 +0800

    change database from sqlite to postgres

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'djangojokes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jokes',
+        'USER': 'postgres',
+        'PASSWORD': 'P@$$w0rd',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
@@ -120,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
