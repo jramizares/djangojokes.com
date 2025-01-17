fe7f7004814843c15cd47955f7088dcd59779ccf
jramizares <jramizares@gmail.com>
2025-01-17 11:43:17 +0800

    Fix for SSL certificate error

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -1,4 +1,6 @@
 import os
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 """
 Django settings for djangojokes project.
--- jobs/views.py
+++ jobs/views.py
@@ -1,4 +1,4 @@
-
+import html
 from django.urls import reverse_lazy
 from django.views.generic import FormView, TemplateView
 
