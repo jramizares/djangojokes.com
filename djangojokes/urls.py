b1371e9bb37740b1f8eec628f57384e330aa621e
jramizares <jramizares@gmail.com>
2025-01-26 23:21:16 +0800

    Adding media settings

--- djangojokes/urls.py
+++ djangojokes/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -29,4 +31,5 @@ urlpatterns = [
     path('jobs/', include('jobs.urls')),
     path('jokes/', include('jokes.urls')),
     path('', include('pages.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
