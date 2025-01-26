7027ca2d0c64c76c820f741630d978953980281b
jramizares <jramizares@gmail.com>
2025-01-26 23:12:22 +0800

    Adding success message to JokeDeleteView

--- jokes/views.py
+++ jokes/views.py
@@ -1,5 +1,6 @@
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
 
@@ -20,9 +21,17 @@ class JokeDeleteView(UserPassesTestMixin, DeleteView):
     model = Joke
     success_url = reverse_lazy('jokes:list')
 
+    def delete(self, requet, *args, **kwargs):
+        result = super().delete(request, *args, **kwargs)
+        return result
+
     def test_func(self):
         obj = self.get_object()
         return self.request.user == obj.user
+    
+    def form_valid(self, form):
+        messages.success(self.request, 'Joke deleted.')
+        return super().form_valid(form)
 
 class JokeDetailView(DetailView):
     model = Joke
